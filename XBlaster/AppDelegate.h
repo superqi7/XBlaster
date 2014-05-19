89971c2d722c3bb1c2d38b5e0ae9c4c368dc14bc
Xiaoqi Liu <xiaoqiliu7@gmail.com>
2014-05-19 10:11:15 -0400

    partical

---
+++ XBlaster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Xblaster
+//
+//  Created by Xiaoqi Liu on 3/18/14.
+//  Copyright (c) 2014 Xiaoqi Liu. All rights reserved.
+//
+
+
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XBlaster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Xblaster
+//
+//  Created by Xiaoqi Liu on 3/18/14.
+//  Copyright (c) 2014 Xiaoqi Liu. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
